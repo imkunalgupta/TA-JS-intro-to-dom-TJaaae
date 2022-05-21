28cdd95dfdc66e6f37a908cc594a8ad61eefaa8f
Kunal <mailto.guptakunal@gmail.com>
2022-05-21 13:10:04 +0530

    finish

--- block-BJaadz/code/script.js
+++ block-BJaadz/code/script.js
@@ -1,11 +1,12 @@
 // Select the h1 element and store it in a variable named heading.
+let heading = document.querySelector('h1');
 
 // Check the typeof heading and log it.
-
+console.log(typeof heading);
 // Change the color of heading to black.
-
+heading.color = "black";
 // Select all the hr elements and store it in a variable named allHrs using querySelectorAll
-
+let allHrs = document.querySelectorAll('hr');
 /* 
 Convert the NodeList returned by querySelectorAll to Array using Array.from() or spread operator and store it in allHrsArray
 
@@ -14,9 +15,9 @@ Array.from converts an array kind of data into array so we can use methods like 
 HINT:
 let allHrsArray = Array.from(allHrs)
 */
-
+let allHrsArray = Array.from(allHrs)
 // Set the border of the all the hr elements to "1px solid tomato"
-
+document.querySelectorAll('hr').border = "1px";
 // Change the background of all the hr to "antiquewhite" using for of loop.
 
 // Change the 'border-radius' of all the hr to "5px" using array.
